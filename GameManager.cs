c5ff22fa9fa7323587ce740d60097cea9b72a6f4
SufianAhmadar <sufian.ahmad00@gmail.com>
2022-03-06 06:30:12 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+public enum States
+{
+    gamecheck,
+    gamestart,
+    gameplay,
+    gamepause,
+    gameover
+}
+public class GameManager : MonoBehaviour
+{
+    public static event gameDelegate gameStart;
+    public static event gameDelegate gamePlay;
+    public static event gameDelegate gamePause;
+    public static event gameDelegate gameOver;
+  
+    
+    public static States gameStates;
+    void Start()
+    {
+        gameStates = States.gamestart;
+    }
+    private void Update()
+    {
+        if (gameStates == States.gamecheck)
+        {
+            return;
+        }
+        else if (gameStates == States.gamestart)
+        {
+            if (gameStart != null)
+            {
+                gameStart();
+            }
+            gameStates = States.gamecheck;
+        }
+        else if (gameStates == States.gameplay)
+        {
+            if (gamePlay != null)
+                gamePlay();
+        }
+        else if (gameStates == States.gamepause)
+        {
+            if (gamePause != null)
+            {
+                gamePause();
+                gameStates = States.gamecheck;
+            }
+        }
+        else if (gameStates == States.gameover)
+        {
+            if (gameOver != null)
+            {
+                gameOver();
+                gameStates = States.gamecheck;
+            }
+        }
+    }
+}
